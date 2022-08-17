4bd73042f1f0b904847ba09d62f592c712a49ddd
baizhi958216 <1475289190@qq.com>
2022-08-17 22:34:35 +0800

    Support generate png local
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,90 @@
 <template>
+  <div class="pornbg-ipt">
+    <input v-model="left" class="pornbg-left" type="text">
+    <input v-model="right" class="pornbg-right" type="text">
+  </div>
+
+  <div class="generate">
+    <button class="generate-btn" @click="generate">Generate</button>
+  </div>
+
   <div class="pornbg">
-    <div class="pornbg-ipt">
-      <input v-model="left" class="pornbg-left" type="text">
-      <input v-model="right" class="pornbg-right" type="text">
-    </div>
-    <div class="pornbg-gen">
-      <div class="gen-left">{{ left }}</div>
-      <div class="gen-right">{{ right }}</div>
-    </div>
+    <div class="gen-left">{{ left }}</div>
+    <div class="gen-right">{{ right }}</div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import html2canvas from 'html2canvas'
+import { saveAs } from 'file-saver';
 let left = ref(''), right = ref('')
+
+const generate = () => {
+  html2canvas(document.querySelector('.pornbg')).then((canvas)=>{
+    canvas.toBlob((blob)=>{
+      saveAs(blob,`${left.value}${right.value}.png`)
+    })
+  })
+}
+
 
 </script>
 
 <style lang="less" scoped>
+.pornbg-ipt {
+  display: flex;
+  justify-content: space-around;
+
+  .pornbg-left,
+  .pornbg-right {
+    background-color: rgba(233, 231, 231, 0.912);
+    height: 40px;
+    width: 40%;
+    border: none;
+    outline: none;
+    text-align: center;
+    font-size: 30px;
+    border-radius: 10px;
+    margin: 30px 0;
+  }
+}
+
+.generate {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &-btn {
+    font-size: 40px;
+    color: orange;
+    border: none;
+    cursor: pointer;
+    background-color: rgb(63, 63, 63);
+    font-family: 'Fira Code';
+    font-weight: 600;
+  }
+}
+
 .pornbg {
   background-color: black;
   width: 800px;
   height: 800px;
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
   display: flex;
-  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  font-size: 70px;
 
-  &-ipt {
-    display: flex;
-    justify-content: space-around;
-
-    .pornbg-left,
-    .pornbg-right {
-      background-color: rgba(255, 255, 255, 0.646);
-      height: 40px;
-      width: 40%;
-      border: none;
-      outline: none;
-      margin-top: 10px;
-      text-align: center;
-      font-size: 30px;
-      border-radius: 10px;
-    }
+  .gen-left {
+    color: white;
+    font-weight: 700;
   }
 
-  &-gen {
-    position: relative;
-    top: 30%;
-    display: flex;
-    font-size: 100px;
-    font-family: 'Fira Code';
-    align-items: center;
-    justify-content: center;
-
-    .gen-left {
-      color: white;
-      font-weight: 700;
-    }
-
-    .gen-right {
-      background-color: orange;
-      font-weight: 700;
-      border-radius: 10px;
-    }
+  .gen-right {
+    background-color: orange;
+    font-weight: 700;
+    border-radius: 10px;
   }
 }
 </style>
